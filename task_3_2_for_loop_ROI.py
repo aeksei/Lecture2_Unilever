15c138480df0d323dff0a2eea234aa0cb339c5db
aeksei <aeksei94@gmail.com>
2021-12-10 15:01:54 +0300

    init

---
+++ task_3_2_for_loop_ROI.py
@@ -0,0 +1,23 @@
+def get_chanel_with_max_roi(chanels):
+    """Найти канал с наибольшим ROI"""
+    # с помощью цикла for и условия if найти канал с наибольшим ROI
+
+
+if __name__ == "__main__":
+    list_chanels = [
+        {
+            "chanel": "yandex",
+            "income": 20000,
+            "spend": 10000,
+        },
+        {
+            "chanel": "instagram",
+            "income": 35000,
+            "spend": 15000,
+        },
+        {
+            "chanel": "google",
+            "income": 10000,
+            "spend": 25000,
+        },
+    ]
