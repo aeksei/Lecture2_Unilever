afe21e6443d15171b04339113b8925b2d98fd833
aeksei <aeksei94@gmail.com>
2021-12-13 20:22:03 +0300

    repalce on todo

--- task_5_1_class_based.py
+++ task_5_1_class_based.py
@@ -2,31 +2,19 @@ FILENAME = "Marketing_Spend.csv"
 
 
 class MarketingSpend:
-    def __init__(self, filename, separate_value=","):
-        self.filename = filename
-        self.separate_value = separate_value
+    def __init__(self):  # todo аргументы для работы с csv файлом
+        # todo атрибуты экземпляра
 
-        self.columns_list = self.get_columns_list()
+        self.columns_list = ...  # todo с помощью метода получить список колонок
 
         self.list_dicts = []
-        self.init_list_dicts()
+        # todo инициализировать список словарей
 
-    def get_columns_list(self):
-        with open(self.filename) as f:
-            return f.readline().split(self.separate_value)
+    # todo метод для получения колонок
 
-    def init_list_dicts(self):
-        with open(self.filename) as f:
-            self.columns_list = f.readline().split(self.separate_value)
-            for line in f:
-                row_values = line.split(self.separate_value)
-                row_dict = {}
-                for index, column in enumerate(self.columns_list):
-                    row_dict[column] = row_values[index]
-                self.list_dicts.append(row_dict)
+    # todo метод для инициализации списока словарей
 
-    def print_list_dicts(self):
-        print(self.list_dicts)
+    # todo метод для печати
 
 
 if __name__ == "__main__":
